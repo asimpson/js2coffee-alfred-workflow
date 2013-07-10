0e729c38dc599217420c6480e8b677484bd0078c
Adam Simpson <adam@heysparkbox.com>
2013-07-10 15:44:57 -0400

    fix: add npm install to script.
    should fix #1

--- js2coffee.sh
+++ js2coffee.sh
@@ -1,6 +1,15 @@
-PATH=$(npm bin):$PATH
-mkdir -p alfredtmp && cd alfredtmp
-pbpaste > tmp.js
-js2coffee tmp.js > tmp.coffee
-cat tmp.coffee | pbcopy
-cd ../ && rm -r alfredtmp
+function convertToCoffee {
+  PATH=$(npm bin):$PATH
+  mkdir -p alfredtmp && cd alfredtmp
+  pbpaste > tmp.js
+  js2coffee tmp.js > tmp.coffee
+  cat tmp.coffee | pbcopy
+  cd ../ && rm -r alfredtmp
+}
+
+if [ -d "node_modules" ]; then
+  convertToCoffee
+else
+  npm install js2coffee
+  convertToCoffee
+fi
